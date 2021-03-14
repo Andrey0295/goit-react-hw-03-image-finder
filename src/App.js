76dbeced67eecb41056e1bf7d71fd41c677c88e5
Andrey0295 <andreevicandrej00@gmail.com>
2021-03-14 10:11:01 +0200

    Начинает работу с запросами

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Heloo React////im react</p>
-    </div>
-  );
+const API_KEY = '19312346-1618d264863fa21be7207d71c';
+
+class App extends Component {
+  state = {
+    images: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=5`,
+      )
+      .then(res => {
+        this.setState({ images: res.data.hits });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Images</h1>
+        <ul>
+          {this.state.images.map(image => (
+            <li key={image.id}>
+              <img src={image.webformatURL} alt="" width="100" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
